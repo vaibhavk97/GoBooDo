b1dd8df741a4e84d1d1d981a4a82a2e337fba9d9
vaibhav.ku <vaibhav.ku@endurance.com>
2019-07-12 23:40:58 +0530

    "first working iteration of Downloader with proxy"

--- GoBook.py
+++ GoBook.py
@@ -1,32 +1,68 @@
+import json
+import random
 import requests
-from io import BytesIO
-from multiprocessing import Pool
-import time
-import asyncio
-from proxybroker import Broker
-from PIL import Image
 from bs4 import BeautifulSoup
+
 
 class  Goboodo:
 
     def __init__(self,id,country="co.in"):
-        # self.linkMap =
         self.id = id
         self.resethead()
         self.page_count=0
+        self.pageLinkDict = {}
+        self.pageList = []
+        self.lastCheckedPage = ""
+        self.obstinatePages = []
+
+    def createPageDict(self,jsonResponse):
+        for pageData in jsonResponse[0]['page']:
+            self.pageList.append(pageData['pid'])
+            self.pageLinkDict[pageData['pid']]={}
+            self.pageLinkDict[pageData['pid']]['src'] = ""
+            self.pageLinkDict[pageData['pid']]['order'] = pageData['order']
 
     def getInitialData(self):
         initUrl = "https://books.google.co.in/books?id="+self.id+"&printsec=frontcover"
         page_data = requests.get(initUrl, headers=self.head)
-        soup = BeautifulSoup(page_data.content,"html5lib")
+        soup = BeautifulSoup(page_data.content, "html5lib")
         scripts = (soup.findAll('script'))
-        print(scripts[6].text)
+        stringResponse = ("["+scripts[6].text.split("_OC_Run")[1][1:-2]+"]")
+        jsonResponse = json.loads(stringResponse)
+        self.createPageDict(jsonResponse)
+        for elem in jsonResponse[3]['page']:
+            page = elem['pid']
+            self.pageList.remove(page)
+            self.pageLinkDict[page]['src'] = elem['src']
 
+    def insertIntoPageDict(self, subsequentJsonData):
+        if(len(self.pageList)==0):
+            return False
+        for pageData in subsequentJsonData['page']:
+            if('src' in pageData.keys() and pageData['src']!=""):
+                lastPage = pageData['pid']
+                self.pageLinkDict[lastPage]['src'] = pageData['src']
+                try:
+                    self.pageList.remove(lastPage)
+                except Exception as e:
+                    pass
+        return True
 
-    def fetchPageLinks(self,page):
-        self.b_url = "https://books.google.co.in/books?id="+str(self.id)+"&lpg=RA1-PA141&pg=PT"+ str(page) +"&jscmd=click3"
-        page_data = requests.get(self.b_url, headers=self.head)
-        return page_data.json()
+    def fetchPageLinks(self,proxy=None):
+        self.resethead()
+        if(proxy):
+            proxy = "https://"+proxy
+            proxyDict = {
+                "http": proxy,
+                "https": proxy,
+            }
+            self.b_url = "https://books.google.co.in/books?id=" + str(self.id) + "&pg=" + str(self.pageList[0]) + "&jscmd=click3"
+            page_data = requests.get(self.b_url, headers=self.head,proxies=proxyDict)
+            return page_data.json()
+        else:
+            self.b_url = "https://books.google.co.in/books?id="+str(self.id)+"&pg="+ str(self.pageList[0]) +"&jscmd=click3"
+            page_data = requests.get(self.b_url, headers=self.head)
+            return page_data.json()
 
     def resethead(self):
         req = requests.get("https://google.com")
@@ -39,43 +75,37 @@ class  Goboodo:
             'Connection': 'close',
             'Cookie': "NID=" + str(req.cookies['NID']),
                     }
+    # def getProxy(self):
 
-    def startload(self,page_number, page_json):
-        self.resethead()
-        for page_data in page_json[u'page']:
-            if len(page_data) > 1:
-                self.page_count +=1
-                try:
-                    link = page_data[u'src']
-                    print(link)
-                    checkIfPageFetched = 0
-                    while checkIfPageFetched == 0:
-                        pageImage = requests.get(link + '&w=1500', headers=self.head)
-                        if len(pageImage.content) == 119974:
-                            print("ip limit over on page " + page_number)
-                            checkIfPageFetched = 0
-                        else:
-                            checkIfPageFetched = 1
-                            print(f'fetched page {page_number+self.page_count}')
-                            im = Image.open(BytesIO(pageImage.content))
-                            im.save(str(page_number+self.page_count)+str(page_data['pid']) + ".png")
-                except Exception as e:
-                    pass
-            else:
-                break
 
 if __name__ == "__main__":
-    elem = Goboodo("AaUzDwAAQBAJ")
-    elem.getInitialData()
-    i = 1
-    # while True:
-    #     page_json = elem.fetchPageLinks(i)
-    #     print()
-    #     for data in page_json[u'page']:
-    #         if(len(data))>1:
-    #             i+=1
-    #             print(data)
-    #
-    #     print(page_json)
-    # elem.startload(0,page_json)
-    # print(elem.b_url)
+    book = Goboodo("NWTwBQAAQBAJ")
+    book.getInitialData()
+    interimData = book.fetchPageLinks()
+    lastFirstList = book.pageList[0]
+    maxPageLimit = 0
+    maxPageLimitHIT = 4
+    proxyFlag = 0
+    while True:
+        if (proxyFlag):
+            print("using proxy")
+            interimData = book.fetchPageLinks("wjvglned-"+str(random.randint(10,800))+":pdlm9oshnhe9@p.webshare.io:80")
+        else:
+            interimData = book.fetchPageLinks()
+        book.insertIntoPageDict(interimData)
+        print(book.pageList)
+        if(maxPageLimit == book.pageList[0]):
+            maxPageLimitHIT-=1
+        if(maxPageLimitHIT==0):
+            maxPageLimitHIT = 4
+            book.obstinatePages.append(book.pageList[0])
+            book.pageList = book.pageList[1:]
+            print("Check_limit_exceeded")
+            print(book.pageLinkDict)
+        if(lastFirstList==book.pageList[0]):
+            maxPageLimit = lastFirstList
+            proxyFlag = 1
+        else:
+            proxyFlag = 0
+        lastFirstList = book.pageList[0]
+
