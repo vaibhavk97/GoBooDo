5fe2bde1b73ee57d2c030f565f177babedef5440
minamotorin <76122224+minamotorin@users.noreply.github.com>
2021-09-28 18:18:44 +0000

    Update makePDF.py for OCR

--- makePDF.py
+++ makePDF.py
@@ -2,6 +2,9 @@ from fpdf import FPDF
 import os
 from PIL import Image
 from tqdm import tqdm
+from pytesseract import image_to_pdf_or_hocr
+import PyPDF2
+from io import BytesIO
 
 class createBook:
 
@@ -23,3 +26,17 @@ class createBook:
         name = str(self.name[:min(10,len(self.name))]).replace(" ","")
         name = ''.join(ch for ch in name if ch.isalnum()) + ".pdf"
         pdf.output(os.path.join(self.path,'Output',name),"F")
+
+    def ocrPdf(self, lang=None):
+        pdf = PyPDF2.PdfFileWriter()
+        for pagePath in tqdm(self.imageNameList):
+            with open(pagePath, 'rb') as ofile:
+                im = Image.open(ofile)
+                page = image_to_pdf_or_hocr(im, lang=lang)
+            pdf.addPage(PyPDF2.PdfFileReader(BytesIO(page)).getPage(0))
+        if not os.path.exists(os.path.join(self.path,'Output')):
+            os.mkdir(os.path.join(self.path,'Output'))
+        name = str(self.name[:min(10,len(self.name))]).replace(" ","")
+        name = ''.join(ch for ch in name if ch.isalnum()) + ".pdf"
+        with open(os.path.join(self.path,'Output',name),'wb') as ofile:
+            pdf.write(ofile)
