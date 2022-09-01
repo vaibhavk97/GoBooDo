9c44cf229aa3d6b68e36828e78bf7990cd9b3297
Simon-Martin Schröder <martin.schroeder@nerdluecht.de>
2022-09-01 14:24:30 +0200

    Retrieve cookies from books.google.*, not google.*

--- GoBooDo.py
+++ GoBooDo.py
@@ -47,7 +47,7 @@ class  GoBooDo:
 
     def resethead(self):
         try:
-            req = requests.get("https://google."+self.country,verify=False)
+            req = requests.get("https://books.google."+self.country,verify=False)
             self.head = {
                 'Host': 'books.google.'+self.country,
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:53.0) Gecko/20100101 Firefox/53.00',
